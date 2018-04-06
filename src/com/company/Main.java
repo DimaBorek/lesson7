1c801cd1d071a25b9a8f550c592c3c0ea5acb2ba
DimaBorek <drackon4ik@gmail.com>
2018-04-06 20:27:26 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,50 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
-
-    public static void main(int[] array) {
-
-
-        int indexOfMax = 0;
-        for (int i = 1; i > array.length ; i++) {
-            if (array[i]>array[indexOfMax]){
-                indexOfMax = i;
-            }
-
-        }
-        return array[indexOfMax];
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = getUserInput(scanner, 1 ,5 );
+        System.out.println(input);
 
     }
 
+   private static int getUserInput(Scanner scanner,int min, int max){
+       int result;
+       while (true){ //бесконечный цикл
+           System.out.println("Введите число от"+min+"до"+max);
+               try {
+                   String stringNumber = scanner.next();
+                   result = Integer.valueOf(stringNumber);
+               }catch (NumberFormatException e) {
+                   System.out.println("Вы ввели не число");
+                   continue;
+               }
+               if(result<min||result>max) {
+                   System.out.println("число не от" + min + "до" + max);
+                   continue;
+               }
+               break;
+               }
+
+       return result;
+   }
+private static int getUserInputRecursion(Scanner scanner,int min,int max){ // метод рекурсия( вызов метода в своем же методе)
+       int result;
+    System.out.println("Введите число от"+min+"до"+max);
+    try{
+        String stringNumber = scanner.next();
+        result = Integer.valueOf(stringNumber);
+    }catch (NumberFormatException e) {
+        System.out.println("Вы ввели не число");
+        return getUserInputRecursion(scanner,min,max);
+    }
+    if(result<min||result>max) {
+        System.out.println("число не от" + min + "до" + max);
+        return getUserInputRecursion(scanner, min, max);
+    }
+    return result;
+
+  }
 }
